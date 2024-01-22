a10a202bd50393ae48510284600878665e5f41e2
mhhasanmehedi <mmmehedi53@gmail.com>
2024-01-22 09:29:05 +0600

    Login page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import LayoutProvider from "@/providers/LayoutProvider";
 import ProgressProvider from "@/providers/ProgressProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,9 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ProgressProvider>
-          <LayoutProvider>{children}</LayoutProvider>
-        </ProgressProvider>
+        <ProgressProvider>{children}</ProgressProvider>
       </body>
     </html>
   );
