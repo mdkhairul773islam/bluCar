6a5891a8fbf0acfc4a3dfe7926ec57017b1b2406
mhhasanmehedi <mmmehedi53@gmail.com>
2024-02-06 07:07:59 +0600

    Add Product Damage Module

--- src/components/damageProduct/validationSchema.js
+++ src/components/damageProduct/validationSchema.js
@@ -1,13 +1,18 @@
 import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string().required().label("Name"),
-  mobile: Yup.number()
-    .typeError("Mobile must be a number")
+  date: Yup.date().required().label("date"),
+  showroom: Yup.string().required().label("showroom"),
+  product_name: Yup.string().required().label("product_name"),
+  stock: Yup.number()
+    .typeError("Stock must be a number")
     .required()
-    .label("Mobile"),
-  address: Yup.string().label("Address"),
-  area: Yup.string().label("Area"),
+    .label("stock"),
+  quantity: Yup.number()
+    .typeError("Quantity must be a number")
+    .required()
+    .label("quantity"),
+  remark: Yup.string().label("remark"),
 });
 
 export default validationSchema;
