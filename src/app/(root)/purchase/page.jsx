c244c7bbaf31b8dd8b6aec0747fe6cd82510c9b1
mhhasanmehedi <mmmehedi53@gmail.com>
2024-02-27 22:06:47 +0600

    Purchase Add page done

---
+++ src/app/(root)/purchase/page.jsx
@@ -0,0 +1,15 @@
+import Panel from "@/components/common/Panel";
+
+export const metadata = {
+  title: "Purchase",
+};
+
+const PurchasePage = () => {
+  return (
+    <>
+      <Panel title="All Purchase"></Panel>
+    </>
+  );
+};
+
+export default PurchasePage;
